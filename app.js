2e6256b6c994989f31f8c55f85bf4df4dfd75c16
arun <40155193+just-arun@users.noreply.github.com>
2019-07-30 17:27:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { json, urlencoded } = require("express");
 const path = require('path');
-const todo = require('./routes');
+const todo = require('./routes/todo');
 const app = express();
 
+require(./config/db);
 app.use(json());
 app.use(urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
